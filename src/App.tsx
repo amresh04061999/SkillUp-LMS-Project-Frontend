0c9d3a6b91435bbd368979278515a9f926028e78
amresh <amresh04061999>
2025-04-04 12:22:50 +0530

    feat:Create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Grid } from '@mantine/core';
-function App() {
-  const [count, setCount] = useState(0)
 
+import './App.css'
+import { Footer } from './layouts/Footer'
+import Header from './layouts/Header'
+import MainLayout from './layouts/MainLayout'
+import { Flex ,Box} from '@mantine/core';
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      <Grid>
-      <Grid.Col span={4}>1</Grid.Col>
-      <Grid.Col span={4}>2</Grid.Col>
-      <Grid.Col span={4}>3</Grid.Col>
-      </Grid>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-        
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <> 
+ <Flex direction="column" h="100vh">
+        <Header />
+        <Box flex={1} w="100%">
+          <MainLayout />
+        </Box>
+        <Footer />
+      </Flex>
     </>
   )
 }
-
 export default App
