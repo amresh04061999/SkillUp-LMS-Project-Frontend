04cf0f82cf8a8acdb05732336c16cf5071b687ab
amresh <amresh04061999>
2025-04-04 15:20:35 +0530

    Feat Basic card design completed

---
+++ src/features/auth/SignUp.tsx
@@ -0,0 +1,11 @@
+import { Box, Flex } from "@mantine/core"
+
+const SignUp =()=>{
+return(
+    <Flex justify={"center"} align={"center"}>
+            <Box>
+                
+            </Box>
+    </Flex>
+)
+}
